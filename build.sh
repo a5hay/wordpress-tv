35b821bf76dc8faede87bac4dac6fb7b708160f7
Ashay Manjure <Ashay.Manjure@fox.com>
2016-05-05 16:39:31 -0700

    adding MIT License as a .txt file and changin name of the package in build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PLUGIN_NAME=plugin.audio.example
+PLUGIN_NAME=plugin.video.wordpresstv
 VERSION=0.0.1
 EXCLUDE_PARAMS="--exclude=*.svn* --exclude=*.git* --exclude=*.DS_Store --exclude=*.idea* --exclude=*.pyc* --exclude=*.sh --exclude=*.md"
 rm -rf "$PLUGIN_NAME-$VERSION.zip"
