fe49becd44a5e8943518bb01266e966869d68aaf
Ashay Manjure <Ashay.Manjure@fox.com>
2016-05-04 19:46:58 -0700

    testing build script

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PLUGIN_NAME=plugin.audio.example
+VERSION=0.0.1
+EXCLUDE_PARAMS="--exclude=*.svn* --exclude=*.git* --exclude=*.DS_Store --exclude=*.idea* --exclude=*.pyc* --exclude=*.sh --exclude=*.md"
+rm -rf "$PLUGIN_NAME-$VERSION.zip"
+mkdir $PLUGIN_NAME
+rsync -av --exclude $EXCLUDE_PARAMS --exclude $PLUGIN_NAME . $PLUGIN_NAME
+zip -r "$PLUGIN_NAME-$VERSION.zip" $PLUGIN_NAME
+rm -rf $PLUGIN_NAME/
